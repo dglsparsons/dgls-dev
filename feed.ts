b5967bf3e967f8fad1a0ea26604e1e12b0080f53
Douglas Parsons <dglsparsons@gmail.com>
2021-08-09 10:13:58 +0100

    Update feed formatting

--- feed.ts
+++ feed.ts
@@ -6,15 +6,15 @@ import { getAllPosts } from './posts'
   console.log(`<?xml version="1.0" encoding="utf-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom">
   <title>Douglas Parsons' Blog</title>
-<subtitle>Feed</subtitle>
-<link href="https://dgls.dev/feed.xml" rel="self"/>
+  <subtitle>Feed</subtitle>
+  <link href="https://dgls.dev/feed.xml" rel="self"/>
   <link href="https://dgls.dev"/>
   <updated>${posts[0].date}</updated>
-<id>https://dgls.dev</id>
+  <id>https://dgls.dev</id>
   <author>
-<name>Douglas Parsons</name>
-<email>dglsparsons@gmail.com</email>
-</author>
+    <name>Douglas Parsons</name>
+    <email>dglsparsons@gmail.com</email>
+  </author>
 ${posts.reduce((acc, post) => {
     return `${acc}
   <entry>
